6c3b9fb5bbf262ff96ad6ec76aa38e78108aab73
Maleesha Herath <malishaherath77@gmail.com>
2024-02-07 19:29:58 +0530

    Add dotenv and mysql dependencies, connect to MySQL database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Mysql database connected!");
+});
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
