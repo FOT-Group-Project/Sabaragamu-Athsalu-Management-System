dd3ca8e7b075cadd0f300a18805d4038e95b709d
Maleesha Herath <malishaherath77@gmail.com>
2024-02-12 18:19:40 +0530

    Refactor Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,18 +9,9 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sabaragamu</span>
+        <span className='px-2 py-1'>Sabaragamu</span>
         Athsalu
       </Link>
-
-      <form>
-        <TextInput
-          type='text'
-          placeholder='Search...'
-          rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
-        />
-      </form>
 
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch/>
