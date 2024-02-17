a60107ed65b3c22a44e00c01f79f8fb9f77f0c87
Maleesha Herath <malishaherath77@gmail.com>
2024-02-17 23:01:27 +0530

    Update SignIn page layout and text

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,13 +6,13 @@ import logo from '../assets/logo.png'
 export default function SignIn() {
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-10'>
         {/* left side */}
         <div className='flex-1'>
           <Link to="/" className=''>
            <img src={logo} class="h-16" alt="Flowbite Logo" />
           </Link>
-          <p className='text-sm mt-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex officiis harum vel magni, tenetur neque officia ducimus debitis atque, animi ratione voluptate. Nihil illo error ducimus sit cum atque illum.</p>
+          <p className='text-sm mt-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex officiis harum vel magni, tenetur neque officia ducimus..</p>
         </div>
 
         {/* right side */}
@@ -26,7 +26,7 @@ export default function SignIn() {
               <Label value='Your Password'/>
               <TextInput type='password' placeholder='***********' id='password'/>
             </div>
-            <Button gradientDuoTone='purpleToBlue' className='w-full'>
+            <Button gradientDuoTone='purpleToBlue' className='w-full mt-2'>
               Sign In
             </Button>
           </form>
