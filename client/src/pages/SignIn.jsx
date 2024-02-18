a0544b8891478d980995868771c0464246cb34ef
Maleesha Herath <malishaherath77@gmail.com>
2024-02-18 18:27:17 +0530

    Complete signin functionality and error handling

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import logo from '../assets/logo.png'
 
 export default function SignIn() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(!formData.username || !formData.password){
+      return setErrorMessage('All fields are required');
+    }
+
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if(res.ok){
+        navigate('/dashboard');
+      }
+
+    }catch(error){
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-10'>
@@ -36,7 +83,7 @@ export default function SignIn() {
                     </>
                     
                   ) : (
-                    'Sign In'
+                    'Log In'
                   ) 
                 }    
             </Button>
