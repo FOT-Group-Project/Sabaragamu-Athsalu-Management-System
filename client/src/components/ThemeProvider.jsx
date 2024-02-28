839e94b05a3529b6d9d9041036f042d3bc08e9af
Maleesha Herath <malishaherath77@gmail.com>
2024-02-28 21:47:08 +0530

    Add min-h-screen class to ThemeProvider div

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,7 @@ export default function ThemeProvider({ children}) {
   const theme = useSelector((state) => state.theme.theme);
   return (
     <div className={theme}>
-      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)]">
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen">
         {children}
       </div>
     </div>
