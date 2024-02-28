40e283a4b2ae50805e7c225ef46b7779cfb7789d
Maleesha Herath <malishaherath77@gmail.com>
2024-02-28 20:27:16 +0530

    Add dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,13 @@ import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
 import logo from '../assets/logo.png'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -18,7 +22,7 @@ export default function Header() {
       </Button>
 
       <div className='flex gap-2 md:order-2' >
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={ () => dispatch(toggleTheme())}>
           <FaMoon/>
         </Button>
         <Link to="/sign-in">
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import themeReducer from "./theme/themeSlice.js";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
