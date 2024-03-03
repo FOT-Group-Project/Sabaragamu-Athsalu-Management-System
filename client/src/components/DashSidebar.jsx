34f5b25cc79e8cc3ac40556d2ff40b80658efc86
Maleesha Herath <malishaherath77@gmail.com>
2024-03-03 16:35:35 +0530

    Add Firebase and Circular Progressbar dependencies, implement image upload functionality in DashProfile component

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -24,6 +24,7 @@ export default function DashSidebar() {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
