2128fe0973d3880e0b063979d9bec8c88bcd6d23
Maleesha Herath <malishaherath77@gmail.com>
2024-03-03 15:05:37 +0530

    Add profile editing functionality to DashProfile component

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,40 @@
+import { Button, TextInput } from "flowbite-react";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.profilepicurl}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-4 border-[lightgray] "
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" color="blue">
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-end mt-5">
+        <sapn className="cursor-pointer">Sign Out</sapn>
+      </div>
+    </div>
+  );
 }
