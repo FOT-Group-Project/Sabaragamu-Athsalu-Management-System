600499a95004041bdc4c0a9b81ad1d6f8d161893
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-03-20 02:53:58 +0530

    Refactor routing to include PrivateRoute component

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user.route");
 const authRoutes = require("./routes/auth.route");
+const productRoutes = require("./routes/products.route"); // products route
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
@@ -15,6 +17,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/addProduct", productRoutes); //products route
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
