3af6be3fb80e2b93e43bc0c31f008d47fe9662e8
Maleesha Herath <malishaherath77@gmail.com>
2024-04-21 22:52:03 +0530

    Refactor DashPOS component to fetch and display products

--- client/src/components/DashPOS.jsx
+++ client/src/components/DashPOS.jsx
@@ -36,9 +36,31 @@ import {
   HiOutlineExclamationCircle,
   HiPlusCircle,
   HiUserAdd,
+  HiOutlineArrowCircleRight,
 } from "react-icons/hi";
 
 export default function DashPOS() {
+  const [products, setProducts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch(`/api/product/getallproducts`);
+      const data = await res.json();
+      if (res.ok) {
+        setProducts(data.products);
+        if (data.product.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 w-full">
       <Breadcrumb aria-label="Default breadcrumb example">
@@ -55,10 +77,53 @@ export default function DashPOS() {
       </h1>
 
       <div className="min-h-screen flex flex-col md:flex-row">
-        <div className="md:w-full bg-slate-700">
-          <h1>Test</h1>
+        <div className="md:w-full mr-5">
+          {products.length > 0 ? (
+            <>
+              <Table hoverable className="shadow-md w-full">
+                <TableHead>
+                  <TableHeadCell>Product Name</TableHeadCell>
+                  <TableHeadCell>SKU</TableHeadCell>
+                  <TableHeadCell>Manufacturer</TableHeadCell>
+                  <TableHeadCell>Price</TableHeadCell>
+                  <TableHeadCell>Quantity</TableHeadCell>
+                  <TableHeadCell></TableHeadCell>
+                  <TableHeadCell>
+                    <span className="sr-only">Edit</span>
+                  </TableHeadCell>
+                </TableHead>
+                {products.map((product) => (
+                  <Table.Body className="divide-y" key={product.id}>
+                    <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                      <TableCell><b>{product.itemName}</b></TableCell>
+                      <TableCell>PD{product.id}</TableCell>
+                      <TableCell>{product.manufacturer}</TableCell>
+                      <TableCell>Rs. {product.itemPrice}</TableCell>
+                      <TableCell>86 </TableCell>
+                      <TableCell></TableCell>
+                      <TableCell>
+                        <Button
+                          onClick={() => {
+                            setOpenModalEdit(true);
+                            setFormData(product);
+                          }}
+                          color="gray"
+                        >
+                          Add
+                          <HiOutlineArrowCircleRight className="ml-3 h-4 w-4" />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  </Table.Body>
+                ))}
+              </Table>
+            </>
+          ) : (
+            <p>You have no store yet!</p>
+          )}
         </div>
-        <div className="md:w-96 bg-slate-100">
+
+        <div className="md:w-96">
           <div className="flex gap-3 justify-between">
             <Button
               className="mb-3"
