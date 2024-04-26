704be1af05e5b1063d5e2c2309ae17cf01150aff
nim3xh <nim3xh@gmail.com>
2024-04-26 22:44:40 +0530

    Add association between CustomerBuyItem and User models

--- api/models/customerbuyitem.js
+++ api/models/customerbuyitem.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      CustomerBuyItem.belongsTo(models.User, {
+        foreignKey: 'id',
+        as: 'Customer',
+      });
     }
   }
   CustomerBuyItem.init({
