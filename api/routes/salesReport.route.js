777d6d03b21b9798021884685bad3dc2fa0c63ce
nim3xh <nim3xh@gmail.com>
2024-04-26 23:59:43 +0530

    Add save function to salesReport.controller.js and addsales route to salesReport.route.js

--- api/routes/salesReport.route.js
+++ api/routes/salesReport.route.js
@@ -5,5 +5,6 @@ const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
 
 router.get('/getsales', verifyToken, salesReportController.showSalesReport);
+router.post('/addsales', verifyToken, salesReportController.save);
 
 module.exports = router;
