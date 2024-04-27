42dd129e52579a511bb85a5e3b359ea31bd69f57
nim3xh <nim3xh@gmail.com>
2024-04-27 18:02:03 +0530

    Refactor DashSalesReport component to improve search functionality

--- client/src/components/DashSalesReport.jsx
+++ client/src/components/DashSalesReport.jsx
@@ -42,16 +42,19 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function DashSalesReport() {
     const { currentUser } = useSelector((state) => state.user);
     const [sales, setSales] = useState([]);
-    const [showMore, setShowMore] = useState(true);
-    const [showModal, setShowModal] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+  
+    //function to handle search input change
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
 
-    const [openModal, setOpenModal] = useState(false);
-    
-    const [formData, setFormData] = useState({});
-
-    const [createLoding, setCreateLoding] = useState(null);
-
-    const [errorMessage, setErrorMessage] = useState(null);
+    //filter sales by customer name,product name,shop name
+    const filterSales = sales.filter((sale) =>
+        sale.Customer.firstname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        sale.Product.itemName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        sale.Shop.shopName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     const fetchSales = async () => {
         try{
@@ -81,53 +84,62 @@ export default function DashSalesReport() {
                 transition={{ duration: 0.5 }}
             >
                 <Breadcrumb aria-label="Default breadcrumb example">
-                    <Breadcrumb.Item href="#" icon={HiHome}>
-                    Home
-                    </Breadcrumb.Item>
+                    <Link to="/dashboard?tab=dash">
+                        <Breadcrumb.Item href="" icon={HiHome}>
+                            Home
+                        </Breadcrumb.Item>
+                    </Link>
                     <Breadcrumb.Item>Shops</Breadcrumb.Item>
                 </Breadcrumb>
                 <h1 className="mt-3 mb-3 text-left font-semibold text-xl">
                     All Sales
                 </h1>
-
-                {currentUser.role == "Admin" ? (
-                    <>
-                    <Table hoverable className="shadow-md w-full">
-                        <TableHead>
-                            <TableHeadCell>Customer Name</TableHeadCell>
-                            <TableHeadCell>Item Name</TableHeadCell>
-                            <TableHeadCell>Shop Name</TableHeadCell>
-                            <TableHeadCell>buy Date Time</TableHeadCell>
-                            <TableHeadCell>Unit Price</TableHeadCell>
-                            <TableHeadCell>Type</TableHeadCell>
-                            <TableHeadCell>Quantity</TableHeadCell>
-                            <TableHeadCell>Amount Paid</TableHeadCell>
-                            {/* <TableHeadCell>Due Amount</TableHeadCell> */}
-                            {/* <TableHeadCell>Total Amount</TableHeadCell> */}
-                        </TableHead>
-                        {sales.map((sale) => (
-                        <Table.Body key={sale.id} className="divide-y">
-                            <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                <TableCell>{sale.Customer.firstname}</TableCell>
-                                <TableCell>{sale.Product.itemName}</TableCell>
-                                <TableCell>{sale.Shop.shopName}</TableCell>
-                                <TableCell>{sale.buyDateTime}</TableCell>
-                                <TableCell>{sale.unitPrice}</TableCell>
-                                <TableCell>{sale.type}</TableCell>
-                                <TableCell>{sale.quantity}</TableCell>
-                                <TableCell>
-                                    {sale.unitPrice * sale.quantity}
-                                </TableCell>
-                                {/* <TableCell>{sale.dueAmount}</TableCell> */}
-                            </TableRow>
-                        </Table.Body>
-                    ))}
-                    </Table>
-                    </>
-                ) : (
-                    <p>You have no Shop yet!</p>
-                )}
-                
+                <div className="flex items-center justify-end">
+                    <div className="flex items-center">
+                            <TextInput
+                                value={searchQuery}
+                                onChange={handleSearchChange}
+                                placeholder="Search"
+                                className="w-72 h-10"
+                            />
+                    </div>
+                </div>
+                <div className="mt-4">
+                {filterSales.length > 0 ? (
+                        <>
+                            <Table hoverable className="shadow-md w-full">
+                                <TableHead>
+                                    <TableHeadCell>Customer Name</TableHeadCell>
+                                    <TableHeadCell>Item Name</TableHeadCell>
+                                    <TableHeadCell>Shop Name</TableHeadCell>
+                                    <TableHeadCell>Buy Date Time</TableHeadCell>
+                                    <TableHeadCell>Unit Price</TableHeadCell>
+                                    <TableHeadCell>Type</TableHeadCell>
+                                    <TableHeadCell>Quantity</TableHeadCell>
+                                    <TableHeadCell>Amount Paid</TableHeadCell>
+                                </TableHead>
+                                {filterSales.map((sale) => (
+                                    <Table.Body key={sale.id} className="divide-y">
+                                        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                            <TableCell>{sale.Customer.firstname}</TableCell>
+                                            <TableCell>{sale.Product.itemName}</TableCell>
+                                            <TableCell>{sale.Shop.shopName}</TableCell>
+                                            <TableCell>{sale.buyDateTime}</TableCell>
+                                            <TableCell>Rs. {sale.unitPrice}</TableCell>
+                                            <TableCell>{sale.type}</TableCell>
+                                            <TableCell>{sale.quantity}</TableCell>
+                                            <TableCell>
+                                                {sale.unitPrice * sale.quantity}
+                                            </TableCell>
+                                        </TableRow>
+                                    </Table.Body>
+                                ))}
+                            </Table>
+                        </>
+                        ) : (
+                            <p>No sales match your search criteria!</p>
+                        )}
+                </div> 
             </motion.div>
             </AnimatePresence>
         </div>
