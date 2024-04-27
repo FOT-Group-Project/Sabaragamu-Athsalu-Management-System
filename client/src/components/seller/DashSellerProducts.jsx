45893db4158bd8656b171dc1b32f0f054f76a259
Maleesha Herath <malishaherath77@gmail.com>
2024-04-27 09:34:11 +0530

    Update password visibility toggle in DashProfile component

--- client/src/components/seller/DashSellerProducts.jsx
+++ client/src/components/seller/DashSellerProducts.jsx
@@ -1,7 +1,260 @@
-import React from 'react'
+import { React, useState, useRef, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+  Avatar,
+  Button,
+  Breadcrumb,
+  Modal,
+  Checkbox,
+  Label,
+  Alert,
+  TextInput,
+  Select,
+  Spinner,
+} from "flowbite-react";
+import { FaUserEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import { HiHome } from "react-icons/hi";
+import { useSelector } from "react-redux";
+import "react-circular-progressbar/dist/styles.css";
+import {
+  HiOutlineExclamationCircle,
+  HiPlusCircle,
+  HiUserAdd,
+} from "react-icons/hi";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function DashSellerProducts() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [products, setProducts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [productIdToDelete, setproductIdToDelete] = useState("");
+  const [stores, setStores] = useState([]);
+
+  const [openModal, setOpenModal] = useState(false);
+  const [openModalEdit, setOpenModalEdit] = useState(false);
+
+  const [formData, setFormData] = useState({});
+
+  const [createUserError, setCreateUserError] = useState(null);
+  const [createLoding, setCreateLoding] = useState(null);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch(`/api/product/getallproducts`);
+      const data = await res.json();
+      if (res.ok) {
+        setProducts(data.products);
+        if (data.product.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const fetchStore = async () => {
+    try {
+      const res = await fetch(`/api/store/getstores`);
+      const data = await res.json();
+      if (res.ok) {
+        setStores(data.stores);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchStore();
+  }, [products.id]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setCreateLoding(true);
+    try {
+      const res = await fetch("/api/product/addproduct", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setCreateUserError(data.message);
+        setCreateLoding(false);
+        return;
+      }
+
+      if (res.ok) {
+        setCreateUserError(null);
+        setCreateLoding(false);
+        setOpenModal(false);
+        fetchProducts();
+      }
+    } catch (error) {
+      // setCreateUserError("Something went wrong");
+      setCreateLoding(false);
+    }
+  };
+
+  const handleSubmitUpdate = async (e) => {
+    e.preventDefault();
+    setCreateLoding(true);
+    console.log(formData.id);
+    try {
+      const res = await fetch(`/api/product/updateproduct/${formData.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setCreateUserError(data.message);
+        setCreateLoding(false);
+        return;
+      }
+
+      if (res.ok) {
+        setCreateUserError(null);
+        setCreateLoding(false);
+        setOpenModalEdit(false);
+        fetchProducts();
+        navigate("/dashboard?tab=store");
+      }
+    } catch (error) {
+      // setCreateUserError("Something went wrong");
+      setCreateLoding(false);
+    }
+  };
+
+  const handleDeleteProduct = async () => {
+    try {
+      const res = await fetch(
+        `/api/product/deleteproduct/${productIdToDelete}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setProducts((prev) =>
+          prev.filter((store) => store.id !== productIdToDelete)
+        );
+        setShowModal(false);
+        fetchProducts();
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div>DashSellerProducts</div>
-  )
+    <div className="p-3 w-full">
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Breadcrumb aria-label="Default breadcrumb example">
+            <Link to="/dashboard?tab=dash">
+              <Breadcrumb.Item href="" icon={HiHome}>
+                Home
+              </Breadcrumb.Item>
+            </Link>
+            <Breadcrumb.Item>Shop Products</Breadcrumb.Item>
+          </Breadcrumb>
+
+          <h1 className="mt-3 mb-5 text-left font-semibold text-xl">
+            Shop Products
+          </h1>
+
+          {products.length > 0 ? (
+            <>
+              <Table hoverable className="shadow-md w-full">
+                <TableHead>
+                  <TableHeadCell>Product Name</TableHeadCell>
+                  <TableHeadCell>SKU</TableHeadCell>
+                  <TableHeadCell>Type</TableHeadCell>
+                  <TableHeadCell>Manufacturer</TableHeadCell>
+                  <TableHeadCell>Store Name</TableHeadCell>
+                  <TableHeadCell>Quantity</TableHeadCell>
+                  <TableHeadCell>Price</TableHeadCell>
+                </TableHead>
+                {products.map((product) => (
+                  <Table.Body className="divide-y" key={product.id}>
+                    <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                      <TableCell>
+                        <b>{product.itemName}</b>
+                      </TableCell>
+                      <TableCell>{product.sku}</TableCell>
+                      <TableCell>{product.itemType}</TableCell>
+                      <TableCell>{product.manufacturer}</TableCell>
+                      <TableCell>{product.store.storeName}</TableCell>
+                      <TableCell>{product.itemQuantity}</TableCell>
+                      <TableCell>Rs. {product.itemPrice}</TableCell>
+                    </TableRow>
+                  </Table.Body>
+                ))}
+              </Table>
+            </>
+          ) : (
+            <p>You have no store yet!</p>
+          )}
+          <Modal
+            show={showModal}
+            onClose={() => setShowModal(false)}
+            popup
+            size="md"
+          >
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Modal.Header />
+              <Modal.Body>
+                <div className="text-center">
+                  <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                  <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                    Are you sure you want to delete this user?
+                  </h3>
+                  <div className="flex justify-center gap-4">
+                    <Button color="failure" onClick={handleDeleteProduct}>
+                      Yes, I'm sure
+                    </Button>
+                    <Button color="gray" onClick={() => setShowModal(false)}>
+                      No, cancel
+                    </Button>
+                  </div>
+                </div>
+              </Modal.Body>
+            </motion.div>
+          </Modal>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 }
