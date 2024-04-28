b20e8b21592a677e35b6dada4b0010d6a687fc93
Shanaka <108541804+shanaka23@users.noreply.github.com>
2024-04-29 04:03:23 +0530

    Add validation for missing data in createDamageProduct function

--- api/controllers/damageproduct.controllers.js
+++ api/controllers/damageproduct.controllers.js
@@ -3,4 +3,9 @@ const models = require("../models");
 const { getStorekeepers } = require("./user.controller");
 
 function createDamageProduct(req, res) {
+    if (!req.body.date || !req.body.quantity || !req.body.storeId || !req.body.itemId) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Some data is missing" });
+    }
 }
