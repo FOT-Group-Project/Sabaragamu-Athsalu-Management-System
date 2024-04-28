b40c5da7a8b57fd8768f019b47b8c9529c2043a6
Shanaka <108541804+shanaka23@users.noreply.github.com>
2024-04-29 03:34:09 +0530

    Refactor DashDamageProduct component to use React hooks and add new imports in DashDamageProduct.jsx

--- client/src/components/DashDamageProduct.jsx
+++ client/src/components/DashDamageProduct.jsx
@@ -1,1 +1,20 @@
 import { React, useState, useRef, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+  Avatar,
+  Button,
+  Breadcrumb,
+  Modal,
+  Checkbox,
+  Label,
+  Alert,
+  TextInput,
+  Select,
+  Spinner,
+} from "flowbite-react";
