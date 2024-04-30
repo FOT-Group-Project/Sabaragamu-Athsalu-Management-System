31ac53070a10b86cf3264d12e4bd71cbb180f738
Shanaka <108541804+shanaka23@users.noreply.github.com>
2024-04-30 11:40:17 +0530

    Refactor storedamageproduct.controllers.js to remove unused code, add deleteDamageProduct function, and update storedamageproduct.route.js

--- api/controllers/storedamageproduct.controllers.js
+++ api/controllers/storedamageproduct.controllers.js
@@ -9,72 +9,13 @@ const e = require("express");
 
 
 
-// function createDamageProduct(req, res) {
-//     if (!req.body.date || !req.body.quantity || !req.body.storeId || !req.body.itemId) {
-//         return res
-//             .status(400)
-//             .json({ success: false, message: "Some data is missing" });
-//     }
-//     if (req.body.quantity.length < 1) {
-//         return res
-//             .status(400)
-//             .json({ success: false, message: "quantity should be more than 1" });
-//     }
-//     if (req.body.storeId.length < 1) {
-//         return res.status(400).json({
-//             success: false,
-//             message: "storeId should be more than 1",
-//         });
-//     }
-//     if (req.body.itemId.length < 1) {
-//         return res.status(400).json({
-//             success: false,
-//             message: "ItemId should be more than 1",
-//         });
-//     }
-//     const damageproduct = {
-//         date: req.body.	date,
-//         quantity: req.body.quantity,
-//         storeId: req.body.storeId,
-//         itemId: req.body.itemId,
-//     };
-//     models.StoreKeepDamageItem.create(damageproduct)
-//         .then((data) => {
-//             res.status(201).json({
-//                 success: true,
-//                 message: "DamageProduct created successfully",
-//                 data: data,
-//             });
-//         })
-//         .catch((err) => {
-//             res.status(500).json({ success: false, message: "Some error occurred" });
-//         });
-// }
-
-//add data storekeepdamageitem table
-// function addDamageProduct(req, res) {
-//     const damageproduct = {
-//         date: req.body.date,
-//         quantity: req.body.quantity,
-//         storeId: req.body.storeId,
-//         itemId: req.body.itemId,
-//     };
-//     models.StoreKeepDamageItem.create(damageproduct)
-//         .then((data) => {
-//             res.status(201).json({
-//                 success: true,
-//                 message: "DamageProduct created successfully",
-//                 data: data,
-//             });
-//         })
-//         .catch((err) => {
-//             res.status(500).json({ success: false, message: "Some error occurred" });
-//         });
-// }
 
 
 
-//get StoreItem from StoreItem table
+
+
+
+//get StoredamageItem from StoredamageItem table
 function getDamageProduct(req, res) {
     models.StoreKeepDamageItem.findAll()
         .then((data) => {
