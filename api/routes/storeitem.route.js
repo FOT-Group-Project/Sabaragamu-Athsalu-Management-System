7bc513cc5a9c80f023d0a2354e3d0371d79e2916
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-04-30 12:07:41 +0530

    Add storeitem.route.js for handling store item routes

---
+++ api/routes/storeitem.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const storeItemController = require("../controllers/storeitem.controller");
+const verifyToken = require("../utils/verifyUser");
+
+const router = express.Router();
+
+router.get("/getstoreitems/:storeKeeperId", verifyToken, storeItemController.getStoresItems);
+router.get("/getstoreitem/:id", verifyToken, storeItemController.getStoresItemId);
+router.put(
+  "/senditem/:id/:storeId/:itemId",
+  verifyToken,
+  storeItemController.sendStoreItemoShop
+);
+
+module.exports = router;
