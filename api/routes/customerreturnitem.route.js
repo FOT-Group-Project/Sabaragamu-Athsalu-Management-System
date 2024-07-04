57885c73cbdb4b2da4d61ae575ea970fd62e3646
nim3xh <nim3xh@gmail.com>
2024-07-04 20:42:33 +0530

    chore: Refactor customer return item routes for better readability

--- api/routes/customerreturnitem.route.js
+++ api/routes/customerreturnitem.route.js
@@ -4,35 +4,11 @@ const verifyToken = require("../utils/verifyUser");
 
 const router = express.Router();
 
-router.post(
-  "/addcustomerreturnitem",
-  verifyToken,
-  customerReturnItemController.save
-);
-router.post(
-  "/addcustomerreturnitems",
-  verifyToken,
-  customerReturnItemController.addreturns
-);
-router.get(
-  "/getreturnsbyshop/:shopId",
-  verifyToken,
-  customerReturnItemController.showReturnSalesByShopId
-);
-router.get(
-  "/getreturnsbycustomer/:customerId",
-  verifyToken,
-  customerReturnItemController.showReturnSalesByCustomerId
-);
-router.get(
-  "/getreturns",
-  verifyToken,
-  customerReturnItemController.showReturnSales
-);
-router.get(
-  "/getreturnsbyitem/:itemId",
-  verifyToken,
-  customerReturnItemController.showReturnSalesByItemId
-);
+router.post("/addcustomerreturnitem",verifyToken,customerReturnItemController.save);
+router.post("/addcustomerreturnitems",verifyToken,customerReturnItemController.addreturns);
+router.get("/getreturnsbyshop/:shopId",verifyToken,customerReturnItemController.showReturnSalesByShopId);
+router.get("/getreturnsbycustomer/:customerId",verifyToken,customerReturnItemController.showReturnSalesByCustomerId);
+router.get("/getreturns",verifyToken,customerReturnItemController.showReturnSales);
+router.get("/getreturnsbyitem/:itemId",verifyToken,customerReturnItemController.showReturnSalesByItemId);
 
 module.exports = router;
