a4fa63471142daffd1e12e934be65fba19c3155e
MaleeshaHe <malishaherath77@gmail.com>
2024-07-08 23:40:57 +0530

    Update server configuration for host and port

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     host: true,
-    port: 5173,
+    port: 80,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
