e6a2749505e3b6a485737f151e692f511b245edd
MaleeshaHe <malishaherath77@gmail.com>
2024-07-09 14:42:51 +0530

    Update proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
+      "/api": "https://backend.sabaragamuathsalu.online",
     },
   },
   plugins: [react()],
